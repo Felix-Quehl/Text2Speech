389a00c30ce54123ae191c23103bfd18bf882521
Felix Quehl <Felix.Quehl@icloud.com>
2022-03-21 22:17:20 +0100

    adding code

---
+++ src/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Globalization;
+using System.IO;
+using System.Speech.Synthesis;
+
+namespace Text2Speech
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var inputFilePath = args[0];
+            var outputFilePath = args[1];
+
+            var text = File.ReadAllText(inputFilePath);
+
+            var synthesizer = new SpeechSynthesizer();
+            synthesizer.SetOutputToDefaultAudioDevice();
+            var builder = new PromptBuilder();
+            var culture = new CultureInfo("en-US");
+            builder.StartVoice(culture);
+            builder.AppendText(text);
+            builder.EndVoice();
+
+            synthesizer.SetOutputToWaveFile(outputFilePath);
+            synthesizer.Speak(builder);
+        }
+    }
+}
